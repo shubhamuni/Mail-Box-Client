eec570da484745d767ee8ef2cb299b90afb6c1f3
shubhampatil.ani@gmail.com <shubhampatil.ani@gmail.com>
2023-03-28 13:36:17 +0530

    Inbox view page

--- src/components/Mail/Mails.js
+++ src/components/Mail/Mails.js
@@ -3,12 +3,14 @@ import { Button } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 import { Link } from "react-router-dom";
 
-function StripedRowExample() {
+function StripedRowExample(props) {
   let number = 1;
   return (
     <Fragment>
       <Button variant="dark">
-        <Link to="/compose">Compose Email</Link>
+        <Link to="/compose" style={{ textDecoration: "none", color: "white" }}>
+          Compose Email
+        </Link>
       </Button>
       <div style={{ margin: "15rem" }}>
         <h2 style={{ textAlign: "center", paddingBottom: "5rem" }}>Inbox</h2>
@@ -17,7 +19,7 @@ function StripedRowExample() {
           <tbody>
             <tr>
               <td>{number++}</td>
-              <td>Mark</td>
+              <td>{props.sender}</td>
             </tr>
             <tr>
               <td>{number++}</td>
